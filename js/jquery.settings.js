e3443a8f397955ecd5869bbc2d98cec4feed1f56
Kevin Venkiteswaran <kevin@visualind.com>
2015-07-03 19:46:33 -0700

    Remove dupe files. Major copy revamp. Reformat with bullets and background images.

--- js/jquery.settings.js
+++ js/jquery.settings.js
@@ -1,31 +1,22 @@
 // Panel Snap settings
 $('body').panelSnap();
 
-//Unslider settings
-$(function() {
-    $('.img-slider').unslider();
-});
+$(window).load(function() {
+    $('.img-slider').unslider({
+        speed: 300,                       //  The speed to animate each slide (in milliseconds)
+        delay: 4000,                      //  The delay between slide animations (in milliseconds)
+        complete: function() {},          //  A function that gets called after every slide animation
+        keys: true,                       //  Enable keyboard (left, right) arrow shortcuts
+        dots: true,                       //  Display dot navigation
+        fluid: false                      //  Support responsive design. May break non-responsive designs
+    });
 
-$(function() {
-    $('.bg-slide').unslider();
-});
-
-$('.img-slider').unslider({
-	speed: 300,               		//  The speed to animate each slide (in milliseconds)
-	delay: 4000,              		//  The delay between slide animations (in milliseconds)
-	complete: function() {},  		//  A function that gets called after every slide animation
-	keys: true,               		//  Enable keyboard (left, right) arrow shortcuts
-	dots: true,               		//  Display dot navigation
-	fluid: false              		//  Support responsive design. May break non-responsive designs
-});
-
-$('.bg-slide').load(function() {
-	$('.bg-slide').unslider({
-		speed: 300,               		//  The speed to animate each slide (in milliseconds)
-		delay: 4000,              		//  The delay between slide animations (in milliseconds)
-		complete: function() {},  		//  A function that gets called after every slide animation
-		keys: true,               		//  Enable keyboard (left, right) arrow shortcuts
-		dots: true,               		//  Display dot navigation
-		fluid: false              		//  Support responsive design. May break non-responsive designs
-	});
+    $('.bg-slide').unslider({
+        speed: 300,                       //  The speed to animate each slide (in milliseconds)
+        delay: 4000,                      //  The delay between slide animations (in milliseconds)
+        complete: function() {},          //  A function that gets called after every slide animation
+        keys: true,                       //  Enable keyboard (left, right) arrow shortcuts
+        dots: true,                       //  Display dot navigation
+        fluid: false                      //  Support responsive design. May break non-responsive designs
+    });
 });
